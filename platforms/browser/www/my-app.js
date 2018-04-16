81e6cee2097d6a729182f1b2c35ea38f808f86b6
Ahmed Heikal <ahmed.k.heikal@gmail.com>
2018-04-16 02:02:02 +0200

    getting data from hardcoded localstorage

--- platforms/browser/www/my-app.js
+++ platforms/browser/www/my-app.js
@@ -51,3 +51,65 @@ var mainView = app.views.create('.view-main');
 if (cordova.platformId == 'android') {
   StatusBar.backgroundColorByHexString("#a42819");
 }
+
+
+document.addEventListener("deviceready", onDeviceReady, false);
+
+function onDeviceReady() {
+  $('.list > ul').html('');
+  if (typeof storage === 'undefined') {
+    var storage = window.localStorage;
+    var value = JSON.stringify(
+      {
+        title: 'task 1',
+        done: '0',
+      },
+      {
+        title: 'task 2',
+        done: '1',
+      },
+      {
+        title: 'task 3',
+        done: '0',
+      },
+      {
+        title: 'task 4',
+        done: '1',
+      },
+      {
+        title: 'task 1 tany',
+        done: '0',
+      }
+      // [
+      // ]
+    );
+    storage.setItem(todos, value);
+  } else {
+    var value = storage.getItem(todos); // Pass a key name to get its value.
+    for (var i = 0; i < todos.items.length; i++) {
+      if (todos.items[i].done == '1')
+        var checked = 'checked';
+      else
+        var checked = '';
+
+
+      $('.list > ul').append(`
+        <li>
+          <label class="item-checkbox item-content">
+            <!-- Checkbox input -->
+            <input type="checkbox"`+ checked +`/>
+            <!-- Checkbox icon -->
+            <i class="icon icon-checkbox"></i>
+            <div class="item-inner">
+              <!-- Checkbox Title -->
+              <div class="item-title">`+ todos.items[i].title +`</div>
+            </div>
+          </label>
+        </li>
+      `);
+    }
+  }
+  // var storage = window.localStorage;
+  // storage.setItem(todos, value) // Pass a key name and its value to add or update that key.
+  // storage.removeItem(key) // Pass a key name to remove that key from storage.
+}
